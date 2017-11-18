53da1590cf1500e0361deaac17e3f8d6be0a47f1
Bjarki Hreinn Viðarsson <Bjarkiv93@gmail.com>
2017-11-18 21:18:52 +0000

    Fancy front page

--- TVH7 - start takki/modeManager.js
+++ TVH7 - start takki/modeManager.js
@@ -3,47 +3,62 @@
 var modeManager = {
   //PRIVATE DATA
   _selectPlayers : [],
+
+
   createmodeManager: function() {
     this._selectPlayers.push(new ModeSelect());
   },
     render: function(ctx) {
+    var background = new Image();
+    background.src = "resizeImageFolder/FPBG.jpg";
+
+    ctx.drawImage(background,0,0); 
 
     function drawBorder(xPos, yPos, width, height, thickness = 2) {
-      ctx.fillStyle = "yellow";
+      ctx.fillStyle = "#72EAE7";
       ctx.fillRect(xPos - (thickness), yPos - (thickness), width + (thickness * 2), height + (thickness * 2));
     }
+
+    ctx.shadowBlur = 50;
+    ctx.shadowColor = "#72EAE7";
     drawBorder(60, 400, 150, 100);
-    ctx.fillStyle = "red";
-    ctx.fillRect(60, 400, 150, 100);
+    ctx.fillStyle = "black";
+    ctx.fillRect(65, 405, 140, 90);
     ctx.fillStyle = "white";
-    ctx.font = "bold 16px consolas";
-    ctx.fillText("1 Player", 85, 450);
+    ctx.font = "bold 40px Bungee Shade";
+    ctx.fillText("1", 117, 460);
     ctx.stroke();
     drawBorder(360, 400, 150, 100);
-    ctx.fillStyle = "red";
-    ctx.fillRect(360, 400, 150, 100);
+    ctx.fillStyle = "black";
+    ctx.fillRect(365, 405, 140, 90);
     ctx.fillStyle = "white";
-    ctx.font = "bold 16px consolas";
-    ctx.fillText("2 Player", 385, 450);
+    ctx.font = "bold 40px Bungee Shade";
+    ctx.fillText("2", 417, 460);
     ctx.stroke();
     drawBorder(660, 400, 150, 100);
-    ctx.fillStyle = "red";
-    ctx.fillRect(660, 400, 150, 100);
+    ctx.fillStyle = "black";
+    ctx.fillRect(665, 405, 140, 90);
     ctx.fillStyle = "white";
-    ctx.font = "bold 16px consolas";
-    ctx.fillText("3 Player", 685, 450);
+    ctx.font = "bold 40px Bungee Shade";
+    ctx.fillText("3", 717, 460);
     ctx.stroke();
     drawBorder(960, 400, 150, 100);
-    ctx.fillStyle = "red";
-    ctx.fillRect(960, 400, 150, 100);
+    ctx.fillStyle = "black";
+    ctx.fillRect(965, 405, 140, 90);
     ctx.fillStyle = "white";
-    ctx.font = "bold 16px consolas";
-    ctx.fillText("4 Player", 985, 450);
+    ctx.font = "bold 40px Bungee Shade";
+    ctx.fillText("4", 1017, 460);
     ctx.stroke();
-    ctx.fillStyle = "yellow";
-    ctx.font = "bold 50px consolas";
-    ctx.fillText("ÞingmannaTron!", 400, 200);
+    ctx.fillStyle = "#72EAE7";
+    ctx.font = "bold 90px Bungee Shade";
+    ctx.fillText("ÞingmannaTron!", 90, 250);
     ctx.stroke();
+    ctx.font = "bold 60px Bungee Shade";
+    ctx.fillText("Veldu fjölda þingmanna", 85, 700);
+
+    ctx.shadowBlur = 0;
+
+    //<link href="https://fonts.googleapis.com/css?family=Black+Ops+One|Bungee+Inline|Bungee+Shade|Monoton|Press+Start+2P|Russo+One" rel="stylesheet">
 
   }
  /* render: function(ctx){

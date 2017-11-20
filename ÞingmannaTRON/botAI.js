5d00bf4ce07b682829b3762a9b9624292b5095c3
IndridiArn <ina23@hi.is>
2017-11-20 18:41:14 +0000

    Update botAI.js

--- ÞingmannaTRON/botAI.js
+++ ÞingmannaTRON/botAI.js
@@ -12,10 +12,11 @@ function botAI(descr) {
   this.reset_rotation = this.rotation;
   this.rTrail.startX = this.cx;
   this.rTrail.startY = this.cy;
-  this.dir = Math.floor((Math.random()*4)+1);
+  this.dir = Math.floor((Math.random() * 4) + 1);
   this.snakeplayer = g_snakePlayer1;
-  this.distance = 0; // mögulega henda þessu í math.random fall.
+  this.distance = 1; // mögulega henda þessu í math.random fall.
   this.startingDir = this.dir;
+  this.enemyPos = g_snakePlayer1.oldTrails;
 }
 
 // Add these properties to the prototype, where they will server as
@@ -25,7 +26,6 @@ botAI.prototype.halfWidth = 10;
 botAI.prototype.halfHeight = 10;
 
 botAI.prototype.update = function(du) {
-
   var botPos = this.rTrail;
   //var playerPos = this.
   var d = Math.sqrt(((botPos.cx - botPos.cx) * (botPos.cx - botPos.cx)) +
@@ -37,78 +37,106 @@ botAI.prototype.update = function(du) {
     silfrid.play();
     if (this.dir === 1) {
       this.distance += 4;
-    if(this.distance > 10){
+      if (this.distance > 10) {
 
-      if (this.cx < 20 && this.cy < 20) {
-        this.dir = 4;
-        console.log("þetta er dir 1 fall 2")
-      }
-      if (this.cx > 1180 && this.cy < 20) {
-        this.dir = 3;
-        console.log("þetta er dir 1 fall 3")
-      }
-
-      if (this.cx > 1180 && this.cy > 980) {
-        this.dir = 3;
-        console.log("þetta er dir 1 fall 5")
-      }
-
-      if (this.cy < 15 && this.cx < 500) {
-        this.dir = 4;
-        console.log("þetta er dir 1 fall 1");
-      }
-      if (this.cy < 15 && this.cx > 500) {
-        this.dir = 3;
-        console.log("þetta er dir 1 fall 6")
-      }
-      //======================================
-      // reglur með collision á eigin trail
-      //======================================
-    }
-
-      if(this.distance > 40){
-      for (i = 0; i < this.oldTrails.length; i++) {
-        if(i % 2 === 0 || i === 0){
-          if((Math.abs(this.cy - this.oldTrails[i].startY)) < 20){
-
-             if(this.cx < canvas.width/2){
-              this.dir = 4;
-              console.log("þetta er dir 1 snákur klessir á snák fallið og fer dir = 4")
+        if (this.cx < 20 && this.cy < 20) {
+          this.dir = 4;
+          console.log("þetta er dir 1 fall 2")
+        } else if (this.cx > 1180 && this.cy < 20) {
+          this.dir = 3;
+          console.log("þetta er dir 1 fall 3")
+        } else if (this.cx > 1180 && this.cy > 980) {
+          this.dir = 3;
+          console.log("þetta er dir 1 fall 5")
+        } else if (this.cy < 15 && this.cx < 500) {
+          this.dir = 4;
+          console.log("þetta er dir 1 fall 1");
+        } else if (this.cy < 15 && this.cx > 500) {
+          this.dir = 3;
+          console.log("þetta er dir 1 fall 6")
+        }
+        //======================================
+        // reglur með collision á óvina trail dir = 1
+        //======================================
+        if (this.distance > 40) {
+          //console.log(this.enemyPos.length);
+          for (j = 0; j < this.enemyPos.length; j++) {
+            if (j % 2 === 0 || j === 0) {
+              if ((Math.abs(this.cx - this.enemyPos[j].startX) < 25) && (Math.abs(this.cy - this.enemyPos[j].startY) < 25)) {
+                this.dir = Math.floor(Math.random() * 3) + 2;
+                //console.log(this.dir);
+                console.log("þetta er dir 2 AI klessir á player1 ")
+                break;
+              }
             }
-            else if(this.cx > canvas.width/2){
-              this.dir = 3;
-              console.log("þetta er dir 1 snákur klessir á snák fallið og fer dir = 3")
+          }
+        }
+
+
+
+        //======================================
+        // reglur með collision á eigin trail
+        //======================================
+
+        if (this.distance > 40) {
+          for (i = 0; i < this.oldTrails.length; i++) {
+            if (i % 2 === 0 || i === 0) {
+              if ((Math.abs(this.cy - this.oldTrails[i].startY)) < 20) {
+
+                if (this.cx < canvas.width / 2) {
+                  this.dir = 4;
+                  console.log("þetta er dir 1 snákur klessir á snák fallið og fer dir = 4")
+                } else if (this.cx > canvas.width / 2) {
+                  this.dir = 3;
+                  console.log("þetta er dir 1 snákur klessir á snák fallið og fer dir = 3")
+                }
+              }
             }
+          }
         }
       }
-    }
-  }
 
 
-      if(this.dir != 1){
-      console.log(this.distance);
-      this.oldTrails.push(this.rTrail);
-      this.rTrail = [];
-      this.rTrail.startX = this.cx;
-      this.rTrail.startY = this.cy;
-      this.distance = 0;
-    }
+      if (this.dir != 1) {
+        console.log(this.distance);
+        this.oldTrails.push(this.rTrail);
+        this.rTrail = [];
+        this.rTrail.startX = this.cx;
+        this.rTrail.startY = this.cy;
+        this.distance = 0;
+      }
     };
     if (this.dir === 2) {
       this.distance += 4;
       if (this.cx <= 20 && this.cy >= 980) {
-        this.dir = 3;
+        this.dir = 4;
         console.log("þetta er dir 2 fall 3")
-      }
-      else if (this.cx > 1175 && this.cy >= 980) {
+      } else if (this.cx > 1175 && this.cy >= 980) {
         this.dir = 3;
         console.log("þetta er dir 2 fall 2")
-      }
-      else if (this.cy >= 980) {
+      } else if (this.cy > 970 && this.cx > 500) {
         this.dir = 3;
         console.log("þetta er dir 2 fall 1")
+      } else if (this.cy > 970 && this.cx < 500) {
+        this.dir = 4;
+        console.log("þetta er dir 2 fall 4")
       }
+      //======================================
+      // reglur með collision á óvina trail dir = 2
+      //======================================
+      if (this.distance > 40) {
+        //console.log(this.enemyPos.length);
+        for (j = 0; j < this.enemyPos.length; j++) {
+          if (j % 2 === 0 || j === 0) {
+            if ((Math.abs(this.cx - this.enemyPos[j].startX) < 25) && (Math.abs(this.cy - this.enemyPos[j].startY) < 25)) {
+              this.dir = Math.floor(Math.random() * 3) + 2;
+              //console.log(this.dir);
+              break;
 
+            }
+          }
+        }
+      }
 
 
 
@@ -116,32 +144,32 @@ botAI.prototype.update = function(du) {
       // reglur með collision á eigin trail
       //======================================
 
-      if(this.distance > 40){
-      for (i = 0; i < this.oldTrails.length; i++) {
-        if(i % 2 === 0 || i === 0){
-          if((Math.abs(this.cy - this.oldTrails[i].startY)) < 20){
-            if(this.cx < canvas.width/2){
-              this.dir = 4;
-              console.log("þetta er dir 2 snákur klessir á snák fallið og fer dir = 4")
+      if (this.distance > 40) {
+        for (i = 0; i < this.oldTrails.length; i++) {
+          if (i % 2 === 0 || i === 0) {
+            if ((Math.abs(this.cy - this.oldTrails[i].startY)) < 20) {
+              if (this.cx < canvas.width / 2) {
+                this.dir = 4;
+                console.log("þetta er dir 2 snákur klessir á snák fallið og fer dir = 4")
+              }
+              if (this.cx > canvas.width / 2) {
+                this.dir = 3;
+                console.log("þetta er dir 2 snákur klessir á snák fallið og fer dir = 3")
+              }
             }
-            if(this.cx > canvas.width/2){
-              this.dir = 3;
-              console.log("þetta er dir 2 snákur klessir á snák fallið og fer dir = 3")
-            }
+          }
         }
       }
-    }
-  }
 
-      if(this.dir != 2){
-      console.log(this.distance);
-      this.oldTrails.push(this.rTrail);
-      this.rTrail = [];
-      this.rTrail.startX = this.cx;
-      this.rTrail.startY = this.cy;
-      this.distance = 0;
-    }
-  };
+      if (this.dir != 2) {
+        console.log(this.distance);
+        this.oldTrails.push(this.rTrail);
+        this.rTrail = [];
+        this.rTrail.startX = this.cx;
+        this.rTrail.startY = this.cy;
+        this.distance = 0;
+      }
+    };
 
     if (this.dir === 3) {
       this.distance += 4;
@@ -149,63 +177,79 @@ botAI.prototype.update = function(du) {
       if (this.cx < 20 && this.cy < 20) {
         this.dir = 2;
         console.log("þetta er dir 3 fall 2")
-      }
-      if (this.cx < 20 && this.cy > 980) {
+      } else if (this.cx < 20 && this.cy > 980) {
         this.dir = 1;
-
-
         console.log("þetta er dir 3 fall 3")
-      }
-
-      if(this.cx < 70 && this.cy < 20){
+      } else if (this.cx < 70 && this.cy < 20) {
         this.dir = 2;
         console.log("þetta er dir 3 fall 5")
+      } else if (this.cx < 50) {
+        this.dir = Math.floor(Math.random() * 2) + 1
       }
 
-      else if (Math.abs((this.snakeplayer.cx - this.cx)) < 15) {
-        this.dir = 1;
-        console.log("þetta er dir 3 fall 4")
+      //else if (Math.abs((this.snakeplayer.cx - this.cx)) < 15) {
+      //  this.dir = 1;
+      //  console.log("þetta er dir 3 fall 4")
+      //}
+      //======================================
+      // reglur með collision á óvina trail dir = 3
+      //======================================
+      if (this.distance > 40) {
+        //console.log(this.enemyPos.length);
+        for (j = 0; j < this.enemyPos.length; j++) {
+          if (j % 2 != 0 && j != 0) {
+            if ((Math.abs(this.cx - this.enemyPos[j].destX) < 25) && (Math.abs(this.cy - this.enemyPos[j].startY) < 25)) {
+              this.dir = Math.floor(Math.random() * 2) + 1;
+              console.log("þetta er dir 3 AI klessir á player1 ")
+              //console.log(this.dir);
+              break;
+            }
+          }
+        }
       }
+
 
 
 
       //======================================
       // reglur með collision á eigin trail dir = 3
       //======================================
-      if(this.distance > 40){
-      for (i = 0; i < this.oldTrails.length; i++) {
-        if(i % 2 != 0 && i != 0){
-          if((Math.abs(this.cx - this.oldTrails[i].startX)) < 20){
-            if(this.cy < canvas.height/2){
-              if(this.startingDir === 1) this.dir = 2;
-              if(this.startingDir === 2)  this.dir = 2;
-              if(this.startingDir === 3) this.dir = 1;
-              if(this.startingDir === 4) this.dir = 1;
-              console.log("þetta er dir 3 snákur klessir á snák fallið of er dir = 2")
+      if (this.distance > 40) {
+        for (i = 0; i < this.oldTrails.length; i++) {
+          if (i % 2 != 0 && i != 0) {
+            if ((Math.abs(this.cx - this.oldTrails[i].startX)) < 20) {
+              if (this.cy < canvas.height / 2) {
+                if (this.startingDir === 1) this.dir = 2;
+                if (this.startingDir === 2) this.dir = 2;
+                if (this.startingDir === 3) this.dir = 1;
+                if (this.startingDir === 4) this.dir = 1;
+                console.log("þetta er dir 3 snákur klessir á snák fallið of er dir = 2")
+                break;
+              }
+              if (this.cy > canvas.height / 2) {
+                if (this.startingDir === 1) this.dir = 2;
+                if (this.startingDir === 2) this.dir = 2;
+                if (this.startingDir === 3) this.dir = 1;
+                if (this.startingDir === 4) this.dir = 1;
+                console.log("þetta er dir 3 snákur klessir á snák fallið of er dir = 1")
+                break;
+              }
             }
-            if(this.cy > canvas.height/2){
-              if(this.startingDir === 1) this.dir = 2;
-              if(this.startingDir === 2) this.dir = 2;
-              if(this.startingDir === 3) this.dir = 1;
-              if(this.startingDir === 4) this.dir = 1;
-              console.log("þetta er dir 3 snákur klessir á snák fallið of er dir = 1")
-            }
+          }
         }
       }
-    }
-  }
 
 
 
-      if(this.dir != 3){
-      console.log(this.distance);
-      this.oldTrails.push(this.rTrail);
-      this.rTrail = [];
-      this.rTrail.startX = this.cx;
-      this.rTrail.startY = this.cy;
-      this.distance = 0;
-    }
-  };
+      if (this.dir != 3) {
+        console.log(this.distance);
+        this.oldTrails.push(this.rTrail);
+        this.rTrail = [];
+        this.rTrail.startX = this.cx;
+        this.rTrail.startY = this.cy;
+        this.distance = 0;
+      }
+    };
 
 
     if (this.dir === 4) {
@@ -213,67 +257,89 @@ botAI.prototype.update = function(du) {
       if (this.cx > 1180 && this.cy < 20) {
         this.dir = 2;
         console.log("þetta er dir 4 fall 1")
-      }
-      if (this.cx > 1180 && this.cy > 980) {
+      } else if (this.cx > 1180 && this.cy > 980) {
         this.dir = 1;
         console.log("þetta er dir 4 fall 2")
-      }
-      if (this.cx < 20  && this.cy > 980) {
+      } else if (this.cx < 20 && this.cy > 980) {
         this.dir = 1;
         console.log("þetta er dir 4 fall 3")
-      }
-      if (this.cx < 20 && this.cy > 980) {
+      } else if (this.cx < 20 && this.cy > 980) {
         this.dir = 2;
         console.log("þetta er dir 4 fall 4")
-      }
-
-      if(this.cx > 1180){
+      } else if (this.cx > 1180) {
         this.dir = 2;
       }
+
+      //======================================
+      // reglur með collision á óvina trail dir = 4
+      //======================================
+      if (this.distance > 40) {
+        //console.log(this.enemyPos.length);
+        for (j = 0; j < this.enemyPos.length; j++) {
+          if (j % 2 != 0 && j != 0) {
+            if ((Math.abs(this.cx - this.enemyPos[j].startX) < 25) && ((Math.abs(this.cy - this.enemyPos[j].startY)) < 25)) {
+              this.dir = Math.floor(Math.random() * 2) + 1;
+              console.log("þetta er dir 4 AI klessir á player1 ")
+              break;
+              //console.log(this.dir);
+            }
+          }
+        }
+      }
+
+
+      //  for (var i = 0; i < this.enemyPos.length; i++) {
+
+      //if((Math.abs(this.cx - this.enemyPos.destX) < 20)&&(Math.abs(this.cy - this.enemyPos.destY)<20))
+      //this.dir = (Math.floor(Math.random()*2)+1);
+      //console.log("þetta er dir 4, AI klessir á leikmann og dir = 2 ")
+
+      //
+      //}
 
 
       //======================================
       // reglur með collision á eigin trail
       //======================================
 
-      if(this.distance > 40){
-      for (i = 0; i < this.oldTrails.length; i++) {
-        if(i % 2 != 0 && i != 0){
-          if((Math.abs(this.cx - this.oldTrails[i].startX)) < 20){
-            if(this.cy < canvas.height/2){
-              if(this.startingDir === 1) this.dir = 1;
-              if(this.startingDir === 2) this.dir = 1;
-              if(this.startingDir === 3) this.dir = 2;
-              if(this.startingDir === 4) this.dir = 1;
+      if (this.distance > 40) {
+        for (i = 0; i < this.oldTrails.length; i++) {
+          if (i % 2 != 0 && i != 0) {
+            if ((Math.abs(this.cx - this.oldTrails[i].startX)) < 20) {
+              if (this.cy < canvas.height / 2) {
+                if (this.startingDir === 1) this.dir = 1;
+                if (this.startingDir === 2) this.dir = 1;
+                if (this.startingDir === 3) this.dir = 2;
+                if (this.startingDir === 4) this.dir = 1;
 
-              console.log("þetta er dir 4 snákur klessir á snák fallið of er dir = 2")
-            }
-            if(this.cy > canvas.height/2){
-              if(this.startingDir === 1) this.dir = 1;
-              if(this.startingDir === 2) this.dir = 1;
-              if(this.startingDir === 3) this.dir = 2;
-              if(this.startingDir === 4) this.dir = 2;
+                console.log("þetta er dir 4 snákur klessir á snák fallið of er dir = 2")
+              }
+              if (this.cy > canvas.height / 2) {
+                if (this.startingDir === 1) this.dir = 1;
+                if (this.startingDir === 2) this.dir = 1;
+                if (this.startingDir === 3) this.dir = 2;
+                if (this.startingDir === 4) this.dir = 2;
 
-              //this.dir = 2;
-              console.log("þetta er dir 4 snákur klessir á snák fallið of er dir = 1")
+                //this.dir = 2;
+                console.log("þetta er dir 4 snákur klessir á snák fallið of er dir = 1")
+              }
             }
+          }
         }
       }
+
+
+
+      if (this.dir != 4) {
+
+        console.log(this.distance);
+        this.oldTrails.push(this.rTrail);
+        this.rTrail = [];
+        this.rTrail.startX = this.cx;
+        this.rTrail.startY = this.cy;
+        this.distance = 0;
+      }
     }
-  }
-
-
-
-      if(this.dir != 4){
-
-      console.log(this.distance);
-      this.oldTrails.push(this.rTrail);
-      this.rTrail = [];
-      this.rTrail.startX = this.cx;
-      this.rTrail.startY = this.cy;
-      this.distance = 0;
-    }
-  }
     /*
         if(canvas.width - this.cx > 1190 && canvas.height - this.cy < 15)
         this.dir = 1;
@@ -326,8 +392,8 @@ botAI.prototype.halt = function() {
   //console.log(this.cx + ": þetta eru X hnit dauða");
   //console.log(this.cy + ": þetta eru Y hnit dauða");
   //console.log(this.rTrail.startX + " "+ this.rTrail.startY);
-  console.log(this.oldTrails[1].destY);
-  console.log(this.oldTrails[1].destX);
+  //console.log(this.oldTrails[1].destY);
+  //console.log(this.oldTrails[1].destX);
   console.log(this.oldTrails);
   var str2 = [];
   for (var i = 0; i < this.oldTrails.length; i++) {
